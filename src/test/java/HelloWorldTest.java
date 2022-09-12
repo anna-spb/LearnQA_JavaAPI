550ae6b342dacbf7d51c0fb2c622acbc4e643e84
anna-spb <vnukova.anna@gmail.com>
2022-09-12 16:13:27 +0300

    My first API test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.response.Response;
+import io.restassured.RestAssured;
+
 
 public class HelloWorldTest {
 
     @Test
 
     public void testHelloWorld(){
-        System.out.println("Hello from Anna with love");
+       Response response = RestAssured
+               .get("https://playground.learnqa.ru/api/get_text")
+               .andReturn();
+      // response.prettyPrint();
+        System.out.println(response.asString());
     }
 }
