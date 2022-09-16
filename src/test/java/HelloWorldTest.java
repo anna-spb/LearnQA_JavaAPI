b2c34c5c8a2587ff49666863b9cfc670f34ec81a
anna-spb <vnukova.anna@gmail.com>
2022-09-16 16:37:44 +0300

    API test with params

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,16 +2,23 @@ import org.junit.jupiter.api.Test;
 import io.restassured.response.Response;
 import io.restassured.RestAssured;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class HelloWorldTest {
 
     @Test
 
-    public void testHelloWorld(){
-       Response response = RestAssured
-               .get("https://playground.learnqa.ru/api/get_text")
-               .andReturn();
-      // response.prettyPrint();
-        System.out.println(response.asString());
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Anna");
+        Response response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+        // System.out.println(response.asString());
     }
 }
