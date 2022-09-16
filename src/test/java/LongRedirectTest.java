3e4f9d9533e2558ea7cbf08a4362c6de3ffce276
anna-spb <vnukova.anna@gmail.com>
2022-09-16 18:59:54 +0300

    Homework with LongRedirectTest

---
+++ src/test/java/LongRedirectTest.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongRedirectTest {
+
+    @Test
+    public void testLongRedirect() {
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        response.prettyPrint();
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+}
