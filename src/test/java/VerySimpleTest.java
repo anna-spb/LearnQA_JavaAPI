8cb7c9d161b8e51f225d0eac15c43c962287c587
anna-spb <vnukova.anna@gmail.com>
2022-09-25 15:49:55 +0300

    Homework with length test

---
+++ src/test/java/VerySimpleTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VerySimpleTest {
+
+    @Test
+    public void lengthPar() {
+        String test = "move your mind! ";
+
+
+
+        assertTrue(test.length() > 15, "Error, length is less than expected");
+
+    }
+}
