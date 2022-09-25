c71cd659755cae198d3edb88f76a95721e161861
anna-spb <vnukova.anna@gmail.com>
2022-09-25 16:16:29 +0300

    Homework with cookie test

---
+++ src/test/java/CookieTest.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CookieTest {
+    @Test
+    public void testCookie() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+        String cookie = response.cookie("HomeWork");
+        System.out.println(cookie);
+
+        assertEquals("hw_value", cookie, "unexpected cookie");
+
+    }
+}
