a2fe18f64d1d58b048371fe353d3133c2c38abcd
anna-spb <vnukova.anna@gmail.com>
2022-09-25 16:22:23 +0300

    Homework with header test

---
+++ src/test/java/HeaderTest.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class HeaderTest {
+
+    @Test
+    public void testHeader() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+
+        String secretHeader = response.getHeader("x-secret-homework-header");
+        System.out.println(secretHeader);
+
+
+        assertEquals("Some secret value", secretHeader, "unexpected header");
+
+    }
+}
+
+
