efdeedf10b3afa8c8d76ffc6bc3a4fe1e8d618b8
anna-spb <vnukova.anna@gmail.com>
2022-09-26 16:14:39 +0300

    Homework with header test

---
+++ src/test/java/UserAgentTest.java
@@ -0,0 +1,47 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class UserAgentTest {
+
+
+    @ParameterizedTest
+   @ValueSource(strings = {
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+    })
+    public void testAgent(String header)  {
+        Map<String, String> header1 = new HashMap<>();
+        header1.put("User-Agent", header );
+        Response response = RestAssured
+                .given()
+                .header("User-Agent",  header1  )
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+
+       response.prettyPrint();
+
+
+
+        //     assertEquals(expectedHeader, header1);
+
+
+
+
+
+    }
+}
